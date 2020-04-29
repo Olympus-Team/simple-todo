6dc8a11bd899d11c0c3d3b2190762057fc27290e
quangnv281098 <quangkm44@gmail.com>
2020-04-29 12:27:30 +0700

    [QuangNV] sample CRUD Task

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize');
+const dbconfig = require('./dbconfig');
 
-const sequelize = new Sequelize( 'todos', 'root', '123456', { host: 'localhost', dialect: 'mysql', operatorsAliases: false});
+const sequelize = new Sequelize( dbconfig.DB, dbconfig.USER, dbconfig.PASSWORD, 
+    { 
+        host: dbconfig.HOST, dialect: dbconfig.dialect, operatorsAliases: false, pool: dbconfig.pool
+    });
 
 module.exports = sequelize;
