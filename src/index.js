fe42c42a431fd3106408639db719f89e4b1e5ad9
Restyled.io <commits@restyled.io>
2020-05-06 08:28:33 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ const route = require("./routes/index");
 const taskRoute = require("./routes/task-route");
 const tasks = require("./database/connection");
 const middleware = require("./middleware/Auth");
-const cors = require('cors');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(cors({origin : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
 app.use("/api", middleware.checkAuthorize);
 
 route.setupRoutes(app);
 taskRoute.setupRoutes(app);
 
-app.listen(PORT, () => { console.log("App is listening on port: " + PORT); });
+app.listen(PORT, () => {
+  console.log("App is listening on port: " + PORT);
+});
--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-const constanst = require('../constants/index');
+const jwt = require("jsonwebtoken");
+const constanst = require("../constants/index");
 
 /**
  * @param {import('express').Request} req
@@ -7,12 +7,12 @@ const constanst = require('../constants/index');
  * @param {import('express').NextFunction} next
  */
 module.exports.checkAuthorize = (req, res, next) => {
-  let bearer = req.get('Authorization');
+  let bearer = req.get("Authorization");
   if (bearer) {
-    let token = bearer.split(' ')[1];
+    let token = bearer.split(" ")[1];
     let payload = jwt.verify(token, constanst.SECRET_KEY);
     req.current_user = payload.email;
-    next()
+    next();
   }
-  return res.status(401).json({message : constanst.AUTHORIZATION});
+  return res.status(401).json({ message: constanst.AUTHORIZATION });
 };
