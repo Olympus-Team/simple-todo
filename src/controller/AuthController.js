cd19f7be6d18dc794f861cf5275ee1a001d7fd7e
phanduc0908 <phanduc0908@gmail.com>
2020-05-06 08:22:09 +0700

    WIP: update authentication

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -5,9 +5,6 @@ const jwt = require('jsonwebtoken');
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-module.exports.checkAuthorize = (req, res, next) => {
-  if (req.token) {
-    next();
-  }
-  res.sendStatus(401);
-};
+module.exports.login = (req, res, next) => {
+
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
-const controller = require("../controller/UserController");
+const controller = require('../controller/UserController');
+const auth = require('../controller/AuthController');
 
+/**
+ * @param {import('express').Express} app
+ */
 module.exports.setupRoutes = (app) => {
-  app.get("/api/v1/users", controller.getListUser);
-  app.get("/api/v1/users/:userId", controller.getUserById);
-  app.post("/api/v1/user", controller.createUser);
-  app.post("/api/v1/user/update", controller.updateUserById);
-  app.delete("/api/v1/user/delete/:userId", controller.deleteUser);
+  app.post('/login', auth.login);
+
+  app.get('/api/v1/users', controller.getListUser);
+  app.get('/api/v1/users/:userId', controller.getUserById);
+  app.post('/api/v1/user', controller.createUser);
+  app.post('/api/v1/user/update', controller.updateUserById);
+  app.delete('/api/v1/user/delete/:userId', controller.deleteUser);
 };
