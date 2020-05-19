f7730c2bd426257aca2522ad6731290e40df83f8
Restyled.io <commits@restyled.io>
2020-05-19 10:50:18 +0000

    Restyled by clang-format

--- src/helpers/AccessEnv.js
+++ src/helpers/AccessEnv.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 exports.accessEnv = (key) => {
-	if (process.env[key] === undefined) {
-		throw new Error(`${key} do not exist in env file !!!`);
-	}
-	return process.env[key];
+  if (process.env[key] === undefined) {
+    throw new Error(`${key} do not exist in env file !!!`);
+  }
+  return process.env[key];
 };
--- src/index.js
+++ src/index.js
@@ -10,15 +10,11 @@ const PORT = env.accessEnv('PORT');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({origin: true}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cors({origin : true}));
 app.use('/api', middleware.checkAuthorize);
-app.get('/demodocker', (req, res) =>{
-  res.send('Dizz me quang');
-});
+app.get('/demodocker', (req, res) => { res.send('Dizz me quang'); });
 
 route.setupRoutes(app);
 
-app.listen(PORT, () => {
-  console.log('App is listening on port: ' + PORT);
-});
+app.listen(PORT, () => { console.log('App is listening on port: ' + PORT); });
